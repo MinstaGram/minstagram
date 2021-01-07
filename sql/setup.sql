c3513ca43215aa9a5feb275b790b612226619c27
DonnyLVu <donnyvu897@gmail.com>
2021-01-07 14:16:52 -0800

    add verfication on endroutes working

--- sql/setup.sql
+++ sql/setup.sql
@@ -8,14 +8,14 @@ CREATE TABLE users(
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   email TEXT NOT NULL,
   password_hash TEXT NOT NULL,
-  profile_photo_url TEXT NOT NULL
+  profile_photo_url TEXT
 
 );
 
 CREATE TABLE posts(
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   user_id BIGINT NOT NULL REFERENCES users(id),
-  photo_url TEXT NOT NULL,
+  photo_url TEXT,
   caption TEXT NOT NULL,
   tags TEXT[]
 
@@ -24,7 +24,25 @@ CREATE TABLE posts(
 CREATE TABLE comments(
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   comment_by BIGINT NOT NULL REFERENCES users(id),
-  post BIGINT NOT NULL REFERENCES posts(id),
+  post_id BIGINT NOT NULL REFERENCES posts(id),
   comment TEXT NOT NULL
 
 );
+
+-- {
+-- "email":"user@test.com",
+-- "password": "password"
+-- }
+
+-- {
+-- "commentBy":"1",
+-- "post": "1",
+-- "comment": "caption for testing comments 1 user"
+-- }
+
+-- {
+-- "userId": "1",
+-- "photoUrl": "password",
+-- "caption": "caption for testing comments 1 user",
+-- "tags": "tag 1"
+-- }
